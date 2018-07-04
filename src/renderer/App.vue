8042bd453cebc3b270fbbd97d48fb7af66beab66
Stefan Dyankov <s.dyankov@gmail.com>
2018-07-04 18:01:07 +0300

    Inital App Scaffolding

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'inv-calc',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
